a066802efe8ee2c4a23e6aab1dbeca2c8e13370b
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-03-15 16:45:14 +0200

    created homepage

---
+++ dist/main.js
@@ -0,0 +1,106 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/homepage.js":
+/*!*************************!*\
+  !*** ./src/homepage.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"intro\": () => (/* binding */ intro),\n/* harmony export */   \"cardSection\": () => (/* binding */ cardSection)\n/* harmony export */ });\nfunction intro() {\r\n  const container = document.getElementById('content')\r\n  const section = container.appendChild(document.createElement('section'))\r\n  section.classList.add('intro', 'h-screen', 'relative', 'bg-dark', 'text-white')\r\n  const h1 = section.appendChild(document.createElement('h1'))\r\n  h1.innerHTML = 'Le Poes'\r\n  const h2 = section.appendChild(document.createElement('h2'))\r\n  h2.innerHTML = 'A Taste Of France'\r\n  const rosemary = section.appendChild(document.createElement('img'))\r\n  rosemary.id = 'rosemary1'\r\n  rosemary.src = './assets/images/rosemary.png'\r\n  const tomatoes = section.appendChild(document.createElement('img'))\r\n  tomatoes.id = 'tomatoes'\r\n  tomatoes.src = './assets/images/tomatoes.png'\r\n  const onions = section.appendChild(document.createElement('img'))\r\n  onions.id = 'onions'\r\n  onions.src = './assets/images/onions.png'\r\n  const garlic = section.appendChild(document.createElement('img'))\r\n  garlic.id = 'garlic'\r\n  garlic.src = './assets/images/garlic.png'\r\n  const dish = section.appendChild(document.createElement('img'))\r\n  dish.id = 'dish'\r\n  dish.src = './assets/images/dish.png'\r\n}\r\n\r\nfunction cardSection() {\r\n  const container = document.getElementById('content')\r\n  const section = container.appendChild(document.createElement('div'))\r\n  section.classList.add('h-screen', 'bg-dark', 'px-32', 'flex', 'xy-center', 'relative')\r\n  const card = section.appendChild(document.createElement('div'))\r\n  card.classList.add('card', 'h-80')\r\n  const img = card.appendChild(document.createElement('div').appendChild(document.createElement('img')))\r\n  img.classList.add('w-full', 'h-full', 'object-cover')\r\n  img.src = './assets/images/ratatouille.jpg'\r\n  const cardTextContainer = card.appendChild(document.createElement('div'))\r\n  cardTextContainer.classList.add('bg-white', 'px-32', 'flex', 'flex-col', 'justify-center')\r\n  const cardTitle = cardTextContainer.appendChild(document.createElement('h2'))\r\n  cardTitle.innerHTML = 'About Us'\r\n  cardTitle.classList.add('text-4xl', 'mb-8', 'font-bold')\r\n  const cardText = cardTextContainer.appendChild(document.createElement('p'))\r\n  cardText.innerHTML = 'Located in Champs-Élysées, Paris, we aim to provide the real French experience. Le Poes serves the most exquisite French cuisines. From our food, to the wine, and even to the wood we use to cook our food. All of our ingredients are locally sourced, and we inspect all of them to ensure they are of the highest of quality.'\r\n  const thyme = section.appendChild(document.createElement('img'))\r\n  thyme.id = 'thyme'\r\n  thyme.src = './assets/images/thyme.png'\r\n  thyme.classList.add('absolute')\r\n}\n\n//# sourceURL=webpack://restaurant/./src/homepage.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _homepage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./homepage */ \"./src/homepage.js\");\n/* harmony import */ var _parallax__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./parallax */ \"./src/parallax.js\");\n\r\n\r\n\r\n(0,_homepage__WEBPACK_IMPORTED_MODULE_0__.intro)()\r\n;(0,_homepage__WEBPACK_IMPORTED_MODULE_0__.cardSection)()\r\n;(0,_parallax__WEBPACK_IMPORTED_MODULE_1__.parallax)()\r\n\n\n//# sourceURL=webpack://restaurant/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/parallax.js":
+/*!*************************!*\
+  !*** ./src/parallax.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"parallax\": () => (/* binding */ parallax)\n/* harmony export */ });\nfunction parallax() {\r\n  window.addEventListener('scroll', () => {\r\n    const dish = document.getElementById('dish')\r\n    const onions = document.getElementById('onions')\r\n    const tomatoes = document.getElementById('tomatoes')\r\n    const rosemary = document.getElementById('rosemary1')\r\n    const thyme = document.getElementById('thyme')\r\n    let scroll = window.pageYOffset;\r\n  \r\n    dish.style.transform = `translateY(${scroll * -0.85}px)`\r\n    onions.style.transform = `translateY(${scroll * -0.4}px)`\r\n    tomatoes.style.transform = `translateY(${scroll * -0.5}px)`\r\n    rosemary.style.transform = `translateY(${scroll * -0.6}px)`\r\n    thyme.style.transform = `translate(0, ${scroll * -0.3}px) rotate(20deg)`\r\n  })\r\n}\n\n//# sourceURL=webpack://restaurant/./src/parallax.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
---
+++ src/homepage.js
@@ -0,0 +1,46 @@
+export function intro() {
+  const container = document.getElementById('content')
+  const section = container.appendChild(document.createElement('section'))
+  section.classList.add('intro', 'h-screen', 'relative', 'bg-dark', 'text-white')
+  const h1 = section.appendChild(document.createElement('h1'))
+  h1.innerHTML = 'Le Poes'
+  const h2 = section.appendChild(document.createElement('h2'))
+  h2.innerHTML = 'A Taste Of France'
+  const rosemary = section.appendChild(document.createElement('img'))
+  rosemary.id = 'rosemary1'
+  rosemary.src = './assets/images/rosemary.png'
+  const tomatoes = section.appendChild(document.createElement('img'))
+  tomatoes.id = 'tomatoes'
+  tomatoes.src = './assets/images/tomatoes.png'
+  const onions = section.appendChild(document.createElement('img'))
+  onions.id = 'onions'
+  onions.src = './assets/images/onions.png'
+  const garlic = section.appendChild(document.createElement('img'))
+  garlic.id = 'garlic'
+  garlic.src = './assets/images/garlic.png'
+  const dish = section.appendChild(document.createElement('img'))
+  dish.id = 'dish'
+  dish.src = './assets/images/dish.png'
+}
+
+export function cardSection() {
+  const container = document.getElementById('content')
+  const section = container.appendChild(document.createElement('div'))
+  section.classList.add('h-screen', 'bg-dark', 'px-32', 'flex', 'xy-center', 'relative')
+  const card = section.appendChild(document.createElement('div'))
+  card.classList.add('card', 'h-80')
+  const img = card.appendChild(document.createElement('div').appendChild(document.createElement('img')))
+  img.classList.add('w-full', 'h-full', 'object-cover')
+  img.src = './assets/images/ratatouille.jpg'
+  const cardTextContainer = card.appendChild(document.createElement('div'))
+  cardTextContainer.classList.add('bg-white', 'px-32', 'flex', 'flex-col', 'justify-center')
+  const cardTitle = cardTextContainer.appendChild(document.createElement('h2'))
+  cardTitle.innerHTML = 'About Us'
+  cardTitle.classList.add('text-4xl', 'mb-8', 'font-bold')
+  const cardText = cardTextContainer.appendChild(document.createElement('p'))
+  cardText.innerHTML = 'Located in Champs-Élysées, Paris, we aim to provide the real French experience. Le Poes serves the most exquisite French cuisines. From our food, to the wine, and even to the wood we use to cook our food. All of our ingredients are locally sourced, and we inspect all of them to ensure they are of the highest of quality.'
+  const thyme = section.appendChild(document.createElement('img'))
+  thyme.id = 'thyme'
+  thyme.src = './assets/images/thyme.png'
+  thyme.classList.add('absolute')
+}
